2430e5d60a8e7b9efa4fba57c1522d08d565f6d6
clever-cr <cleverumuhuza@gmail.com>
2022-11-02 21:01:10 +0200

    add strategies

--- pages/strategy_page.py
+++ pages/strategy_page.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+# This page will display list of all strategies
+
+print("STRATEGIES")
+print("=============")
+print("1.Time Management")
+print("2.Procrastination")
+print("3.Personal study")
+print("4.External and extensive research")
+print("5.Reflective practice")
+print("6.Team work")
+print("7.Process Thinking")
+print("8.Journaling")
+
