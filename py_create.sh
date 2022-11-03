5e5c9088d24a6fc2e3d72a3576c7d44de2b1b063
VBamurange <113381610+VBamurange@users.noreply.github.com>
2022-11-03 12:31:58 +0200

    py_create.sh

---
+++ py_create.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+"""this is where the code for creating the credentials page
+we will add a input box for the student's name and email and after they put this, they will be able to access their data and use the application.
