5da528269cd1f3255ef27126e7426e1dc513a1d7
Tito-Olisakwe <t.olisakwe@alustudent.com>
2022-11-23 14:36:20 +0200

    reupdating pages

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+# This page will display list of all subjects
+# This page will display list of profiles
+# This page will display list of all strategies
+# This page will display list of average
+# This page will display list of users to navigate all pages.
